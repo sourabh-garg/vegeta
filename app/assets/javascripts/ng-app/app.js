5aaa1b0f735be6d4446791e73d2acfbe3527db36
Rails study <souabhgrg713@gmail.com>
2016-05-10 01:31:30 +0530

    s

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,61 @@
+var app = angular.module('myapp', ['ui.router'])
+
+          .config(function config($stateProvider){
+           
+           $stateProvider.state("first", {
+           	url : "/first",
+           	controller : "FirstCtrl as first",
+           	templateUrl: "first.html"
+           })
+
+           .state("second", {
+           	url : "/second",
+           	controller : "SecondCtrl as second",
+           	templateUrl : "second.html"
+           })
+
+          })
+          .directive('elem', function () {
+            return {
+              restrict: 'E',
+              templateUrl: "templates/second.html"
+            };
+          })
+          .filter('reverse', function(){
+          
+          return function(text){
+            return text.split('').reverse().join('');
+          }
+
+          })
+          .service("greeting", function Greeting(){
+
+           	var greeting =  this;
+
+           	greeting.message = 	"Default";
+           })
+          .service('Data', [function () {
+
+            return {message: "You are a awesome"}
+          }])
+          .controller('FirstCtrl', function($scope, greeting, Data){
+
+            var first = this;
+            first.greeting = greeting;
+
+            first.data = Data;
+
+            first.reversed = function(message){
+              return message.split('').reverse().join('');
+
+            };
+
+          })
+          .controller('SecondCtrl', function($scope, greeting){
+
+            var second = this;
+            second.greeting = greeting;
+          });
+    
+
+    
